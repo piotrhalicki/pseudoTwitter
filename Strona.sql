48373b90c6bed00017001f7030159c3a892c8145
Piotr Halicki <Piotr_Halicki@wp.pl>
2015-04-05 02:47:13 +0200

    Utworzone tabele, dodawanie i wyświetlanie przyjaciół, dodawanie komentarzy

--- Strona.sql
+++ Strona.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Czas generowania: 30 Mar 2015, 03:14
+-- Czas generowania: 04 Kwi 2015, 22:38
 -- Wersja serwera: 5.6.21
 -- Wersja PHP: 5.5.19
 
@@ -23,14 +23,83 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `Friends`
+--
+
+CREATE TABLE IF NOT EXISTS `Friends` (
+`friends_id` int(11) NOT NULL,
+  `friend1_id` int(11) DEFAULT NULL,
+  `friend2_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `Friends`
+--
+
+INSERT INTO `Friends` (`friends_id`, `friend1_id`, `friend2_id`) VALUES
+(33, 7, 16),
+(34, 7, 5),
+(35, 9, 11),
+(36, 9, 10),
+(37, 9, 16),
+(38, 9, 18),
+(39, 9, 5),
+(40, 9, 12),
+(41, 9, 5),
+(42, 9, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `Komentarze`
+--
+
+CREATE TABLE IF NOT EXISTS `Komentarze` (
+`koment_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  `koment_tresc` varchar(60) DEFAULT NULL,
+  `koment_data` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `Posts`
 --
 
 CREATE TABLE IF NOT EXISTS `Posts` (
-`posts_id` int(11) NOT NULL,
-  `tresc` varchar(140) NOT NULL,
-  `data` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+`post_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `tresc` varchar(140) CHARACTER SET utf8 COLLATE utf8_polish_ci DEFAULT NULL,
+  `data` datetime DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `Posts`
+--
+
+INSERT INTO `Posts` (`post_id`, `user_id`, `tresc`, `data`) VALUES
+(1, NULL, 'xxx', '0000-00-00 00:00:00'),
+(2, NULL, 'xxx', '2015-02-02 21:22:22'),
+(4, 9, 'xxx', '2015-02-02 21:22:25'),
+(5, 9, 'xxx', '2013-02-02 21:22:25'),
+(6, 7, 'dgfdg', '2014-02-04 12:43:22'),
+(11, 7, 'Pierwszy post!', NULL),
+(12, 7, 'dada', NULL),
+(14, 7, 'bla bla', '2015-04-02 00:10:28'),
+(16, 7, 'Drugi kit\r\n:)', '2015-04-02 00:22:09'),
+(21, 9, 'k!t k!t k!t', '2015-04-02 00:30:47'),
+(34, 12, 'Tere fere', '2015-04-02 01:37:47'),
+(35, 7, 'kit', '2015-04-02 01:45:21'),
+(37, 14, 'to\r\nb?\r\ndzie\r\nkit\r\nwie\r\nlo\r\npo\r\nzio\r\nmo\r\nwy\r\n', '2015-04-02 02:25:06'),
+(39, 14, 'a polskie znaczki? ??????ó??', '2015-04-02 02:28:19'),
+(44, 7, 'jsfkjdlkasjkd', '2015-04-02 13:48:04'),
+(46, 7, 'Ä…Ä™', '2015-04-02 13:48:27'),
+(49, 7, 'dsjdisjd', '2015-04-02 14:25:29'),
+(50, 7, 'dsjdisjd', '2015-04-02 14:25:36'),
+(51, 7, 'dkfslkfmsdklvmÄ…Ä…Ä…Ä…', '2015-04-02 21:49:55'),
+(52, 7, 'dkfslkfmsdklvmÄ…Ä…Ä…Ä…', '2015-04-02 21:50:05');
 
 -- --------------------------------------------------------
 
@@ -43,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `User` (
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(60) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 --
 -- Zrzut danych tabeli `User`
@@ -51,22 +120,51 @@ CREATE TABLE IF NOT EXISTS `User` (
 
 INSERT INTO `User` (`id`, `name`, `email`, `password`) VALUES
 (5, 'Test2', 'Test2@test.pl', '$2y$11$349yPECgvi/imeiQEpoQA.lVwtDHzn8ANVxpTvWqlHCnKD5IkPzwK'),
-(6, 'qwerty', 'qwerty@qwerty.pl', '$2y$11$8LdD9m1/uvUW8A2jVlswKe.NPjQNcN3ASPdVFQ0A6PHrd/.VYmltK'),
 (7, 'aaa', 'aaa@aaa.pl', '$2y$11$jI8JCxWtbik8kal3TOo9POcjYL7ojsbUvJC9zp0V3YFn0fBHwUclO'),
-(8, 'bbb', 'bbb@bbb.pl', '$2y$11$ijt3aio36vWPYTvZ0WMUW.vPAhQ8znFfagzCu7OFS116ktmWbrmP.'),
 (9, 'ccc', 'ccc@ccc.pl', '$2y$11$w0vVkqIiXRT.Se9NcM3EtuQ75QaMwIQ2fQMxLv8Jp1NEaG9iZdeES'),
 (10, 'abc', 'abc@abc.pl', '$2y$11$DN53vPvZeo8SfeWyLRKtr.I49vzVbGLeaQZBgw60HrP.XuxAhK4HS'),
-(11, 'zzz', 'zzz@zzz.pl', '$2y$11$ktYIwdQu.T0N8Ds9PdEJKO3Ht2trUX3PsHLbO9pscdhQd3727cV2y');
+(11, 'zzz', 'zzz@zzz.pl', '$2y$11$ktYIwdQu.T0N8Ds9PdEJKO3Ht2trUX3PsHLbO9pscdhQd3727cV2y'),
+(12, 'xxx', 'xxx@xxx.pl', '$2y$11$t2rkKPs5056SzsYnAgDrXODxI.wz.Ox.KlDjNcCHY8Qq.EvkGqX0q'),
+(14, 'qqq', 'qqq@qqq.pl', '$2y$11$wJjmhBmHDYSmR3M.eU1BX.hWy8lI/2e/k1FhAoTrknffj0r9yFDD2'),
+(15, 'uuu', 'uuu@uuu.pl', '$2y$11$4gT761AytmfIxr6J4zGOMezfF6eP8171IZxVWvFXhM2X7CVIQaO3S'),
+(16, 'fff', 'fff@fff.pl', '$2y$11$3.1d5BOrVqpswyf2H.ToX.M8WctMV2U8V74BG0o2t7l8gEBuMarAS'),
+(18, '123', '123@123.pl', '$2y$11$5so8t8/mDjpR8jbHA1lNEueDjbn9TQUZO7N7k8zgZ0q64p6Olsh1K');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `Wiadomosci`
+--
+
+CREATE TABLE IF NOT EXISTS `Wiadomosci` (
+`wiado_id` int(11) NOT NULL,
+  `od_id` int(11) NOT NULL,
+  `do_id` int(11) NOT NULL,
+  `wiado_tresc` varchar(255) DEFAULT NULL,
+  `wiado_data` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indeksy dla zrzutów tabel
 --
 
 --
+-- Indexes for table `Friends`
+--
+ALTER TABLE `Friends`
+ ADD PRIMARY KEY (`friends_id`), ADD KEY `friend1_id` (`friend1_id`), ADD KEY `friend2_id` (`friend2_id`);
+
+--
+-- Indexes for table `Komentarze`
+--
+ALTER TABLE `Komentarze`
+ ADD PRIMARY KEY (`koment_id`), ADD UNIQUE KEY `koment_data` (`koment_data`), ADD KEY `user_id` (`user_id`), ADD KEY `post_id` (`post_id`);
+
+--
 -- Indexes for table `Posts`
 --
 ALTER TABLE `Posts`
- ADD PRIMARY KEY (`posts_id`), ADD UNIQUE KEY `tresc` (`tresc`), ADD UNIQUE KEY `data` (`data`);
+ ADD PRIMARY KEY (`post_id`), ADD UNIQUE KEY `data` (`data`), ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `User`
@@ -75,28 +173,70 @@ ALTER TABLE `User`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`), ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `Wiadomosci`
+--
+ALTER TABLE `Wiadomosci`
+ ADD PRIMARY KEY (`wiado_id`), ADD UNIQUE KEY `wiado_data` (`wiado_data`), ADD KEY `od_id` (`od_id`), ADD KEY `do_id` (`do_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `Friends`
+--
+ALTER TABLE `Friends`
+MODIFY `friends_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=43;
+--
+-- AUTO_INCREMENT dla tabeli `Komentarze`
+--
+ALTER TABLE `Komentarze`
+MODIFY `koment_id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT dla tabeli `Posts`
 --
 ALTER TABLE `Posts`
-MODIFY `posts_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=53;
 --
 -- AUTO_INCREMENT dla tabeli `User`
 --
 ALTER TABLE `User`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
+--
+-- AUTO_INCREMENT dla tabeli `Wiadomosci`
+--
+ALTER TABLE `Wiadomosci`
+MODIFY `wiado_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Ograniczenia dla zrzutów tabel
 --
 
 --
+-- Ograniczenia dla tabeli `Friends`
+--
+ALTER TABLE `Friends`
+ADD CONSTRAINT `Friends_ibfk_1` FOREIGN KEY (`friend1_id`) REFERENCES `User` (`id`) ON DELETE CASCADE,
+ADD CONSTRAINT `Friends_ibfk_2` FOREIGN KEY (`friend2_id`) REFERENCES `User` (`id`) ON DELETE CASCADE;
+
+--
+-- Ograniczenia dla tabeli `Komentarze`
+--
+ALTER TABLE `Komentarze`
+ADD CONSTRAINT `Komentarze_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE,
+ADD CONSTRAINT `Komentarze_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `Posts` (`post_id`) ON DELETE CASCADE;
+
+--
 -- Ograniczenia dla tabeli `Posts`
 --
 ALTER TABLE `Posts`
-ADD CONSTRAINT `Posts_ibfk_1` FOREIGN KEY (`posts_id`) REFERENCES `User` (`id`) ON DELETE CASCADE;
+ADD CONSTRAINT `Posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE;
+
+--
+-- Ograniczenia dla tabeli `Wiadomosci`
+--
+ALTER TABLE `Wiadomosci`
+ADD CONSTRAINT `Wiadomosci_ibfk_1` FOREIGN KEY (`od_id`) REFERENCES `User` (`id`),
+ADD CONSTRAINT `Wiadomosci_ibfk_2` FOREIGN KEY (`do_id`) REFERENCES `User` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- profil.php
+++ profil.php
@@ -1,8 +1,6 @@
 <?php
-//	require 'header.php';
-?>
 
-<?php
+// Powitanie
 
 if (isset($_COOKIE["k!tter"])) {
 	echo '<h1 style="text-align: center">', ucfirst($_COOKIE['k!tter']), ", witaj na k!tterze! :)", '</h1>';
@@ -11,9 +9,34 @@ else {
 	echo '<h1 style="text-align: center">', ucfirst($_SESSION['user_name']), ", witaj na k!tterze! :)", '</h1>';
 };
 
+										// OBSŁUGA METODY 'POST'
+										// Kasowanie
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	if(isset($_POST['deleteUser'])) {
+		$conn->query ("DELETE FROM User WHERE id=".$_SESSION['user_id']); // poprawić składnię - $_SESSION powinno byc w {} ?
+		echo "profil skasowany";
+	}
+										// Dodawanie Postów
+	
+	if (isset($_POST['przy_k!ttuj'])) {
+		$data = date('Y-m-d H:i:s');
+		$conn->query ("insert into Posts (user_id, tresc, data) values ('{$_SESSION['user_id']}', '{$_POST['post']}', '$data')");
+		echo 'kit dodany';
+	}
+										// Komentowanie Postów	
+	
+	if(isset($_POST['komentK!tt'])) {
+		$data = date('Y-m-d H:i:s');
+		$conn->query ("insert into Komentarze (user_id, post_id, koment_tresc, koment_data) values ('{$_SESSION['user_id']}', '{$_POST['post_ID']}', '{$_POST['koment']}', '$data')");
+		echo "Komentarz dodany";
+		}
+};
+
 ?>
 
 <br>
+										<!-- DANE PROFILU -->
 
 <div style="text-align: center">
 	<p>
@@ -24,12 +47,29 @@ else {
 		<strong>Emalia:</strong> <?php echo $_SESSION['user_mail'] ?><br>
 		<strong>Hasłord: </strong>znasz tylko Ty ;)<br>
 		<p><input type="submit" name="modyfikacja" value="mody_k!ttuj profil"><br>(Soon...)<br></p>
+		
+		<?php
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			if(isset($_POST['modyfikacja'])) {
+			echo "<form method='POST' action='#'>", 
+			"<fieldset>",
+			"<strong>", "Login: ", "</strong>", "<br>",
+			"<strong>", "e-mail: ", "</strong>", "<br>",
+			"<input type='submit' name='wyslijWiado' value='Wyślij k!ttomość'>", "<br>",
+			"<input type='submit' name='deleteFriend' value='Usuń przyk!ttaciela'>", "<br>",
+			"</fieldset>",
+			"</form>",
+			"<br>";
+			}
+			};
+		?>
 	</p>
 		</fieldset>
 	</p>
 </div>
 
 <br>
+										<!-- DODAWANIE POSTÓW -->
 
 <div style="text-align: center">
 	<form method="POST" action="#">
@@ -51,6 +91,7 @@ else {
 </div>
 
 <br>
+										<!-- OSTATNI POST -->
 
 <div style="text-align: center">
 	<p>
@@ -59,7 +100,7 @@ else {
 	<p>
 <?php
 
-$sql = "SELECT * from Posts WHERE user_id=".$_SESSION['user_id']." ORDER BY data DESC LIMIT 1";
+$sql = "SELECT * FROM Posts WHERE user_id=".$_SESSION['user_id']." ORDER BY data DESC LIMIT 1";
 $result = $conn->query($sql);
 
 	if ($result->num_rows > 0) {
@@ -67,15 +108,19 @@ $result = $conn->query($sql);
 		echo "<fieldset>",
 		"<strong>", "Treść: ", "</strong>", "<em>".$row["tresc"]."</em>", "<br>",
 		"<strong>", "Data: ", "</strong>", "<em>".$row["data"]."</em>", "<br>",
+		"<form method='post' action=".$_SERVER['PHP_SELF'].">",
+		"<textarea type='text' name='koment'></textarea><br>",
+		"<input type='hidden' name='post_ID' value=".$row['post_id'].">",
+		"<input type='submit' name='komentK!tt' value='Skomentuj'><br>",
+		"</form>",
 		"<input type='submit' name='deleteK!tt' value='Usuń k!tt!'>", "<br>",
-		"</fieldset>",
-		"<br>";
+		"</fieldset>";
+		var_dump($_POST['komentK!tt']);
 	}
 }
 else {
 	echo ("<strong>Brak k!ttów!</strong><br><br>");
 };
-
 ?>
 	</p>
 		</fieldset>
@@ -83,6 +128,7 @@ else {
 </div>
 
 <br>
+										<!-- WIADOMOŚCI -->
 
 <div style="text-align: center">
 	<p>
@@ -98,19 +144,48 @@ else {
 </div>
 
 <br>
+										<!-- ZNAJOMI -->
 
 <div style="text-align: center">
 	<p>
 		<fieldset>
 		<legend><h3>< Znajomi k!tterzy ></h3></legend>
 	<p>
-		tu bedą znajomi k!tterzy
+		<?php 
+		$sql = "SELECT * FROM Friends F1 
+				JOIN User ON F1.friend1_id=User.id 
+				JOIN User U2 ON U2.id=F1.friend2_id 
+				WHERE friend1_id=".$_SESSION['user_id'].""; 
+		$result = $conn->query ($sql);
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		// $_SESSION['friend_id'] = $row['id'];
+		// var_dump($_SESSION['friend_id']);
+		
+		echo "<form method='POST' action='#'>", 
+			"<fieldset>",
+			"<strong>", "Login: ", "</strong>", "<em>".$row["name"]."</em>", "<br>",
+			"<strong>", "e-mail: ", "</strong>", "<em>".$row["email"]."</em>", "<br>",
+			"<input type='hidden' name='ID' value=".$row['id'].">",
+			"<input type='hidden' name='login' value=".$row['name'].">",
+			"<input type='submit' name='wyslijWiado' value='Wyślij k!ttomość'>", "<br>",
+			"<input type='submit' name='deleteFriend' value='Usuń przyk!ttaciela'>", "<br>",
+			"</fieldset>",
+			"</form>",
+			"<br>";
+		}
+}
+else {
+	echo ("<strong>Jeszcze nie masz przyk!ttaciół! :(</strong><br><br>");
+}
+?>
 	</p>
 		</fieldset>
 	</p>
 </div>
 
 <br>
+										<!-- USUWANIE PROFILU -->
 
 <div style="text-align: center">
 	<form method="POST" action="#">
@@ -125,24 +200,3 @@ else {
 	</p>
 	</form>
 </div>
-
-<?php 
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	if(isset($_POST['deleteUser'])) {
-		$conn->query ("DELETE FROM User WHERE id=".$_SESSION['user_id']); // poprawić składnię - $_SESSION powinno byc w {} ?
-		echo "profil skasowany";
-	}
-
-	if (isset($_POST['przy_k!ttuj'])) {
-		$data = date('Y-m-d H:i:s');
-		$conn->query ("insert into Posts (user_id, tresc, data) values ('{$_SESSION['user_id']}', '{$_POST['post']}', '$data')");
-		echo 'kit dodany';
-	}
-};
-
-?>
-
-<?php
-//	require 'footer.php';
-?>
